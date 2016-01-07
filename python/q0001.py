d437699cd587bee809c6778d4dedabefa0c66256
faceless garfield <thefaceless819@sina.com>
2016-01-07 11:30:18 +0800

    Another method

--- python/q0001.py
+++ python/q0001.py
@@ -4,11 +4,21 @@ import uuid
 import random
 
 def generatingCouponCode(num):
-    cList = []
+    couponList = []
+    ranSeed = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
     for i in range(0,num):
         #generating by uuid method
-        cList.append(uuid.uuid1())
-    return cList
+        #couponList.append(uuid.uuid1())
+        #generating by other method
+        randn = random.sample(ranSeed,16)
+        couponCode = ''
+        for j in range(0,len(randn)):
+            if ((j+1) % 4 == 0 and (j+1)!=len(randn)) :
+               couponCode = couponCode + randn[j] + '-'
+            else :
+               couponCode = couponCode + randn[j]
+        couponList.append(couponCode)
+    return couponList
 
 if __name__ == '__main__':
     couponList = generatingCouponCode(200)
