77bd9d7efc29cb9ccef8bf5e22277e89802543b7
faceless garfield <thefaceless819@sina.com>
2016-01-11 09:22:44 +0800

    q0005 done!

---
+++ python/q0005.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import os
+import Image
+import re
+
+def archivePic(picPath):
+    try :
+        if picPath[-1] != '/':
+            picPath = picPath+'/'
+        picFileList = os.listdir(picPath)
+        size = (1136,640)
+        for i in picFileList:
+            im = Image.open(picPath+i)
+            im.thumbnail(size)
+            im.save(picPath+i[:-4]+'tb.jpg') 
+    except Exception, e:
+        print e
+    
+
+if __name__ == '__main__':
+        archivePic('/Users/penggarfield/Code/practice/python/trialPhotos/')
